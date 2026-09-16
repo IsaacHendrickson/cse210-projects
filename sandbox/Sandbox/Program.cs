55bd02474237685875ff11246a6f2fe791fdfdc1
Isaac <isaac.t.hendrickson@gmail.com>
2026-09-16 09:20:35 -0600

    Hola

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string name = "John";
+        Console.WriteLine("Hi " + name + "!");
     }
 }
